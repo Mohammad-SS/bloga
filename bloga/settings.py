16824151d32197eb6a1a5ac6246c67c7b9c76a56
سید محمد باقر عظیمی <m.azimi@esra.local>
2022-01-18 14:44:28 +0330

    connected to psql

--- bloga/settings.py
+++ bloga/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+#     User Apps :
 ]
 
 MIDDLEWARE = [
@@ -77,8 +78,12 @@ WSGI_APPLICATION = 'bloga.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "tdudjonu",
+        "USER" : "tdudjonu" ,
+        "PASSWORD" : "4IXJG44_flSMS03CJ0iWrfp5koV9S4Ng" ,
+        "HOST" : "rosie.db.elephantsql.com" ,
+        'PORT': 5432
     }
 }
 
